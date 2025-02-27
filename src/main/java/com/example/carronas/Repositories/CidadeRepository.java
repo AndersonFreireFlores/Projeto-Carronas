55cf87089b6fa4daf435fd45831fba5d2e94640f
AndersonFreireFlores <andersonfflores75@gmail.com>
2025-02-27 16:02:12 -0300

    resolvi problema com os repositories

--- src/main/java/com/example/carronas/Repositories/CidadeRepository.java
+++ src/main/java/com/example/carronas/Repositories/CidadeRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface CidadeRepository extends JpaRepository<UUID, Cidade> {
+public interface CidadeRepository extends JpaRepository<Cidade,UUID> {
 }
